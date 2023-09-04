907602c465ac2dae07de3379f10fb29206468179
little_hobbit <rusikarm3@gmail.com>
2023-09-04 18:25:48 +0300

    add hash algorithm interface

--- src/hash_algorithm.h
+++ src/hash_algorithm.h
@@ -1,10 +1,21 @@
 #ifndef __HASH_ALGORITHM_H_EY22VN0ZRBNP__
 #define __HASH_ALGORITHM_H_EY22VN0ZRBNP__
 
+#include <vector>
+
 namespace hash {
 
 enum class HashFunction { md5, crc32, sha256 };
 
-}
+using Block = std::vector<std::byte>;
+
+class HashAlgorithm {
+ public:
+  virtual ~HashAlgorithm() = default;
+
+  virtual auto hash(const Block &block) noexcept -> std::vector<std::byte> = 0;
+};
+
+}  // namespace hash
 
 #endif  // __HASH_ALGORITHM_H_EY22VN0ZRBNP__
