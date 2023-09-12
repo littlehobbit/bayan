af704a098644932723e263a824761029ba46352b
little_hobbit <rusikarm3@gmail.com>
2023-09-12 09:06:45 +0300

    add sha256 hashing algorithm

---
+++ src/hash/sha256.h
@@ -0,0 +1,30 @@
+#ifndef __SHA256_H_UZT876JPW1EP__
+#define __SHA256_H_UZT876JPW1EP__
+
+#include <vector>
+
+#include <picosha2.h>
+
+#include "hash_algorithm.h"
+
+namespace hash {
+
+class Sha256 final : public HashAlgorithm {
+ public:
+  auto hash(const Block &block) noexcept -> std::vector<std::uint8_t> override {
+    std::vector<std::uint8_t> hash;
+
+    constexpr auto SHA256_HASH_SIZE = 32;
+    hash.resize(SHA256_HASH_SIZE);
+
+    // NOLINTNEXTLINE
+    const auto begin = reinterpret_cast<const std::uint8_t *>(block.data());
+    const auto end = begin + block.size();  // NOLINT
+    picosha2::hash256(begin, end, hash.begin(), hash.end());
+    return hash;
+  }
+};
+
+}  // namespace hash
+
+#endif  // __SHA256_H_UZT876JPW1EP__
