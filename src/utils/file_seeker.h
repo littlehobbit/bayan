b9c489614ad4111539550a04e5ceb67e64b0e60b
little_hobbit <rusikarm3@gmail.com>
2023-09-23 16:28:30 +0300

    remove `utils.h`, add `FileSeeker` class

---
+++ src/utils/file_seeker.h
@@ -0,0 +1,79 @@
+#ifndef __FILE_SEEKER_H_8NUO95F8EK6N__
+#define __FILE_SEEKER_H_8NUO95F8EK6N__
+
+#include <algorithm>
+#include <iostream>
+#include <optional>
+#include <regex>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/directory.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+
+namespace utils {
+
+class FileSeeker {
+ public:
+  FileSeeker(std::vector<boost::filesystem::path> scan_paths,
+             std::vector<boost::filesystem::path> ignore_paths)
+      : _scan_paths{std::move(scan_paths)},
+        _ignore_paths{std::move(ignore_paths)} {}
+
+  auto recursive_find(std::size_t scan_level, std::size_t minimal_size,
+                      const std::optional<std::string> &mask = std::nullopt)
+      -> std::vector<boost::filesystem::path> {
+    namespace fs = boost::filesystem;
+
+    std::vector<fs::path> paths{};
+
+    for (const auto &directory : _scan_paths) {
+      // TODO: extract to `scan_directory`
+      // TODO: append results of scan to `paths`
+
+      auto directory_iterator = fs::recursive_directory_iterator{directory};
+      for (auto &&entry : directory_iterator) {
+        if (directory_iterator.depth() == (scan_level + 1) ||
+            is_ignored(entry)) {
+          directory_iterator.disable_recursion_pending();
+          continue;
+        }
+
+        if (fs::is_regular_file(entry) &&
+            fs::file_size(entry) >= minimal_size) {
+          if (mask.has_value() &&
+              !std::regex_search(entry.path().filename().string(),
+                                 std::regex{*mask})) {
+            continue;
+          }
+
+          paths.push_back(std::move(entry));
+        }
+      }
+    }
+
+    return paths;
+  }
+
+ private:
+  bool is_ignored(const boost::filesystem::path &path) const noexcept {
+    return std::any_of(
+        _ignore_paths.cbegin(), _ignore_paths.cend(),
+        [&](auto &ignored) { return is_subpath(ignored, path); });
+  }
+
+  bool is_subpath(const boost::filesystem::path &base,
+                  const boost::filesystem::path &entry) const noexcept {
+    const auto mismatch_pair =
+        std::mismatch(entry.begin(), entry.end(), base.begin(), base.end());
+    return mismatch_pair.second == base.end();
+  }
+
+  std::vector<boost::filesystem::path> _scan_paths;
+  std::vector<boost::filesystem::path> _ignore_paths;
+};
+
+}  // namespace utils
+
+#endif  // __FILE_SEEKER_H_8NUO95F8EK6N__
--- tests/test_file_seeker.cpp
+++ tests/test_file_seeker.cpp
@@ -9,7 +9,10 @@
 
 #include <gtest/gtest.h>
 
-#include "utils.h"
+#include "utils/file_seeker.h"
+
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock-more-matchers.h>
 
 #if not defined(TEST_DIR_PATH)
 #define TEST_DIR_PATH "tests"
@@ -21,52 +24,57 @@ using namespace ::testing;
 
 class FilesSeekerTest : public ::testing::Test {
  public:
-  void SetUp() override { ASSERT_TRUE(fs::exists(test_dir_path)); }
+  void SetUp() override { ASSERT_TRUE(fs::exists(TEST_DIR)); }
 
-  fs::path test_dir_path = fs::path{TEST_DIR_PATH} / "for_tests";
+  fs::path TEST_DIR = fs::path{TEST_DIR_PATH} / "for_tests";
 };
 
 // NOLINTNEXTLINE
-TEST_F(FilesSeekerTest, RecursiveFind_NoDepth_NoMask) {
-  auto res = utils::recursive_find(test_dir_path, 0, 0);
+TEST_F(FilesSeekerTest, Find_NoDepth_NoMask_NoIgnore) {
+  utils::FileSeeker seeker{{TEST_DIR}, {}};
+  auto res = seeker.recursive_find(0, 0);
 
   std::vector<fs::path> expected = {
-      test_dir_path / "file1.txt",
-      test_dir_path / "file2.bat",
-      test_dir_path / "file3.sh",
+      TEST_DIR / "file1.txt",
+      TEST_DIR / "file2.bat",
+      TEST_DIR / "file3.sh",
   };
+  ASSERT_THAT(res, UnorderedElementsAreArray(expected));
+}
+
+// NOLINTNEXTLINE
+TEST_F(FilesSeekerTest, Find_NoDepth_NoMask_NoIgnore_WithSize1) {
+  utils::FileSeeker seeker{{TEST_DIR}, {}};
+  auto res = seeker.recursive_find(0, 1);
+
+  std::vector<fs::path> expected = {TEST_DIR / "file1.txt"};
   ASSERT_EQ(expected, res);
 }
 
 // NOLINTNEXTLINE
-TEST_F(FilesSeekerTest, RecursiveFind_NoDepth_NoMask_WithSize1) {
-  auto res = utils::recursive_find(test_dir_path, 0, 1);
-
-  std::vector<fs::path> expected = {test_dir_path / "file1.txt"};
-  ASSERT_EQ(expected, res);
-}
-
-// NOLINTNEXTLINE
-TEST_F(FilesSeekerTest, RecursiveFind_With1Depth_NoMask) {
-  auto res = utils::recursive_find(test_dir_path, 1, 0);
+TEST_F(FilesSeekerTest, Find_With1Depth_NoMask_NoIgnore) {
+  utils::FileSeeker seeker{{TEST_DIR}, {}};
+  auto res = seeker.recursive_find(1, 0);
 
   std::vector<fs::path> expected = {
-      test_dir_path / "file1.txt",
-      test_dir_path / "file2.bat",
-      test_dir_path / "file3.sh",
-      test_dir_path / "subdirectory" / "file4.sh",
-      test_dir_path / "subdirectory" / "file5.data",
+      TEST_DIR / "file1.txt",
+      TEST_DIR / "file2.bat",
+      TEST_DIR / "file3.sh",
+      TEST_DIR / "subdirectory" / "file4.sh",
+      TEST_DIR / "subdirectory" / "file5.data",
+      TEST_DIR / "ignore" / "ignored.txt",
   };
-  ASSERT_EQ(expected, res);
+  ASSERT_THAT(res, UnorderedElementsAreArray(expected));
 }
 
 // NOLINTNEXTLINE
-TEST_F(FilesSeekerTest, RecursiveFind_WithFullDepth_WithMask) {
+TEST_F(FilesSeekerTest, Find_WithFullDepth_WithMask_NoIgnore) {
   constexpr auto FULL_TEST_DEPTH = 4;
-  auto res =
-      utils::recursive_find(test_dir_path, FULL_TEST_DEPTH, 0, ".*\\.hex");
 
-  std::vector<fs::path> expected = {test_dir_path / "subdirectory" /
+  utils::FileSeeker seeker{{TEST_DIR}, {}};
+  auto res = seeker.recursive_find(FULL_TEST_DEPTH, 0, ".*\\.hex");
+
+  std::vector<fs::path> expected = {TEST_DIR / "subdirectory" /
                                     "third_subdirectory" / "file8.hex"};
   ASSERT_EQ(expected, res);
 }
@@ -76,12 +84,63 @@ TEST_F(FilesSeekerTest, OnNoSuchPath_ThrowError) {
   fs::path bad_dir_path = fs::path{TEST_DIR_PATH} / "dummy_bad_path";
   ASSERT_FALSE(fs::exists(bad_dir_path));
 
-  ASSERT_THROW(utils::recursive_find(bad_dir_path, 0, 0),
+  utils::FileSeeker seeker{{bad_dir_path}, {}};
+  ASSERT_THROW(seeker.recursive_find(0, 0),
                boost::filesystem::filesystem_error);
 }
 
 // NOLINTNEXTLINE
 TEST_F(FilesSeekerTest, OnBadRegex_ThrowError) {
-  ASSERT_THROW(utils::recursive_find(test_dir_path, 0, 0, "ba(d"),
-               std::regex_error);
+  utils::FileSeeker seeker{{TEST_DIR}, {}};
+  ASSERT_THROW(seeker.recursive_find(0, 0, "ba(d"), std::regex_error);
+}
+
+// NOLINTNEXTLINE
+TEST_F(FilesSeekerTest, Find_With1Depth_WithIgnore_NoMask) {
+  const auto INGORE_DIR = TEST_DIR / "ignore";
+  utils::FileSeeker seeker{{TEST_DIR}, {INGORE_DIR}};
+  auto res = seeker.recursive_find(1, 0);
+
+  std::vector<fs::path> expected = {
+      TEST_DIR / "file1.txt",
+      TEST_DIR / "file2.bat",
+      TEST_DIR / "file3.sh",
+      TEST_DIR / "subdirectory" / "file4.sh",
+      TEST_DIR / "subdirectory" / "file5.data",
+  };
+  ASSERT_THAT(res, UnorderedElementsAreArray(expected));
+}
+
+// NOLINTNEXTLINE
+TEST_F(FilesSeekerTest, OnFind_InIgnoredPath_ReturnNothing) {
+  const auto INGORED_DIR = TEST_DIR / "ignore";
+  const auto SUB_INGORED_DIR = TEST_DIR / "ignore" / "sub_ignored";
+
+  utils::FileSeeker seeker{{SUB_INGORED_DIR}, {INGORED_DIR}};
+  auto res = seeker.recursive_find(1, 0);
+
+  ASSERT_THAT(res, IsEmpty());
+}
+
+// NOLINTNEXTLINE
+TEST_F(FilesSeekerTest, OnNoScanDirectory_ReturnNothing) {
+  utils::FileSeeker seeker{{}, {}};
+  auto res = seeker.recursive_find(1, 0);
+  ASSERT_THAT(res, IsEmpty());
+}
+
+// NOLINTNEXTLINE
+TEST_F(FilesSeekerTest, OnFind_WithMultipleScanDirectories_ReturnAllFiles) {
+  const auto FIRST_SCAN_DIR = TEST_DIR / "subdirectory" / "second_subdirectory";
+  const auto SECOND_SCAN_DIR = TEST_DIR / "subdirectory" / "third_subdirectory";
+
+  utils::FileSeeker seeker{{FIRST_SCAN_DIR, SECOND_SCAN_DIR}, {}};
+  auto res = seeker.recursive_find(1, 0);
+
+  std::vector<fs::path> expected = {
+      FIRST_SCAN_DIR / "file6.foo",
+      FIRST_SCAN_DIR / "file7.bar",
+      SECOND_SCAN_DIR / "file8.hex",
+  };
+  ASSERT_THAT(res, UnorderedElementsAreArray(expected));
 }
